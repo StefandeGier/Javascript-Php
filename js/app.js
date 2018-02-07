c2273d15cbdee76e0ef538624cb7ce42c523611e
StefandeGier <99040150@mydavinci.nl>
2018-02-07 19:16:43 +0100

    Changes js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ for(let subject of subjects){
 }
 
 var choices = [];
-choices[0] = "eens";
 var numb = 0;
 
   display();
@@ -33,14 +32,26 @@ function back() {
   }
 }
 
+function end(){
+  if (numb == subjects.length) {
+    alert("Dat was em dan");
+  }
+}
+
 function agree(){
+  choices[numb] = "eens";
+  next();
 
 }
 
 function disagree(){
-
+  choices[numb] = "oneens";
+  next();
+  end();
 }
 
 function neither(){
-
+  choices[numb] = "geen van beide";
+  next();
+  end();
 }
