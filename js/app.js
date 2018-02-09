794fa847793bd0a53f26079214f198209b2cea5b
StefandeGier <99040150@mydavinci.nl>
2018-02-09 18:51:42 +0100

    Major changes js

--- js/app.js
+++ js/app.js
@@ -3,12 +3,23 @@
 for(let subject of subjects){
 	//console.log(subject);
 }
-
+var bweight = false;
 var choicesweight = [];
 var choices = [];
 var numb = 0;
 
 choicesWeight()
+
+function displayParties(){
+  bweight = true;
+  alert(bweight);
+  document.getElementById("nextweight").style.display = "none";
+  document.getElementById("selectparties").style.display = "none";
+  document.getElementById("title").innerHTML = "Kies partijen";
+  document.getElementById("statement").innerHTML = "Kies de partijen die u in uw test wilt";
+
+
+}
 
 function displayStart(test){
   if (test == true) {
@@ -30,18 +41,24 @@ function display(){
   document.getElementById("buttons").style.display = "block";
   document.getElementById("selectparties").style.display = "none";
   document.getElementById("start").style.display = "none";
+  document.getElementById("nextweight").style.display = "none";
+
 
   console.log(numb);
   console.log(subjects.length);
   console.log(choices);
+  console.log(choicesweight);
 }
 
 function displayWeight(){
+  bweight = false;
+  alert(bweight);
   document.getElementById("title").innerHTML = "Zijn er onderwerpen die u extra belangrijk vindt?";
   document.getElementById("statement").innerHTML = "Aangevinkte stellingen tellen extra mee bij het berekenen van het resulaat.";
   document.getElementById("statement").style.fontSize = "20px";
   document.getElementById("buttons").style.display = "none";
   document.getElementById("selectparties").style.display = "block";
+  document.getElementById("nextweight").style.display = "block";
   choicesWeight();
 }
 
@@ -52,13 +69,26 @@ function choicesWeight(){
     var i;
     for (i = 0; i < subjects.length; i++) {
 
-        text += "<h4>"+subjects[i].title + '<input style="margin-left:10px;" type="checkbox" id="myField" data-id="" value="">'+"</h4>"+"<br>"+"<hr>";
-        document.getElementById("test").innerHTML = text;
-
+        text += "<h4>"+subjects[i].title + "<input style='margin-left:10px;' type='checkbox' class='myField' value='"+ i +"'></h4><br><hr>";
+        document.getElementById("checkbox").innerHTML = text;
 
     }
   }
 
+function weightCalc(){
+  var checkedValue = null;
+  var inputElements = document.getElementsByClassName('myField');
+  for(var i=0; inputElements[i]; ++i){
+        if(inputElements[i].checked){
+             checkedValue = inputElements[i].value;
+             alert(checkedValue);
+             choicesweight[checkedValue] = "Extra punten";
+
+        }
+
+  }
+   displayParties();
+}
 
 
 
@@ -68,20 +98,23 @@ function next() {
     numb++;
     display();
   }
+
   else if (numb == subjects.length-1) {
     alert("diplayResults");
-
+    numb = 7
     displayWeight();
   }
 }
 
 function back() {
-
+  if (bweight == true) {
+    displayWeight()
+  }
   if (numb > 0) {
     numb--;
+
     display();
   }else if(numb == 0) {
-
     displayStart(true);
   }
 }
